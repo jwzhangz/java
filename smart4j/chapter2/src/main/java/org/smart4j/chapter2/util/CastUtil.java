90b6353bba38ba0170ebc3454dbb6a6d61abe97b
jwzhang <8wy143288@163.com>
2017-07-02 20:23:20 +0800

    update

--- smart4j/chapter2/src/main/java/org/smart4j/chapter2/util/CastUtil.java
+++ smart4j/chapter2/src/main/java/org/smart4j/chapter2/util/CastUtil.java
@@ -4,4 +4,55 @@ package org.smart4j.chapter2.util;
  * Created by Think on 2017/7/2.
  */
 public class CastUtil {
+    public static String castString(Object obj) {
+        return CastUtil.castString(obj, "");
+    }
+
+    public static String castString(Object obj, String defaultValue) {
+        if (obj == null) {
+            return defaultValue;
+        }
+        return String.valueOf(obj);
+    }
+
+    public static double castDouble(Object obj){
+        return castDouble(obj, 0);
+    }
+
+    public static double castDouble(Object obj, double defaultValue){
+        double doubleVal = defaultValue;
+        if (obj == null) {
+            return doubleVal;
+        }
+
+        String strVal = CastUtil.castString(obj);
+        if (StringUtil.isEmpty(strVal)) {
+            return doubleVal;
+        }
+
+        try {
+            doubleVal = Double.parseDouble(strVal);
+        } catch (NumberFormatException e){
+            doubleVal = defaultValue;
+        }
+
+        return doubleVal;
+    }
+
+    public static long castLong(Object obj) {
+        return CastUtil.castLong(obj, 0);
+    }
+
+    public static long castLong(Object obj, long defaultValue) {
+        long longVal = defaultValue;
+
+        if (obj == null) {
+            return longVal;
+        }
+
+        String strValue = CastUtil.castString(obj);
+
+        if (StringUtil)
+    }
+
 }
