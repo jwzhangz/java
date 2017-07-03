4b316dee02c0321d21014e10930125722d3b1634
jwzhangz <8wy143288@163.com>
2017-07-04 01:10:53 +0800

    update

--- smart4j/chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ smart4j/chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -1,16 +1,94 @@
 package org.smart4j.chapter2.service;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.locks.Condition;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.util.PropsUtil;
 
 
 /**
  * Created by AA on 2017/6/28.
  */
 public class CustomerService {
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
+    static {
+        Properties conf = PropsUtil.loadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("can not load jdbc driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            LOGGER.error("get connection fail", e);
+        }
+        return conn;
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                LOGGER.error("failed to close connection", e);
+            }
+        }
+    }
+
     public List<Customer> getCustomerList(String keyword){
-        //TODO
+        Connection connection = null;
+
+        try {
+            List<Customer> customerList = new ArrayList<Customer>();
+            String sql = "SELECT * FROM customer";
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getLong("id"));
+                customer.setName(rs.getString("name"));
+                customer.setContact(rs.getString("contact"));
+                customer.setTelephone(rs.getString("telephone"));
+                customer.setEmail(rs.getString("email"));
+                customer.setRemark(rs.getString("remark"));
+                customerList.add(customer);
+            }
+            return customerList;
+        } catch (SQLException e) {
+            LOGGER.error("excute sql failed", e);
+        }finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    LOGGER.error("close connect failer", e);
+                }
+            }
+        }
         return null;
     }
 
--- smart4j/chapter2/src/main/java/org/smart4j/chapter2/util/CastUtil.java
+++ smart4j/chapter2/src/main/java/org/smart4j/chapter2/util/CastUtil.java
@@ -52,7 +52,59 @@ public class CastUtil {
 
         String strValue = CastUtil.castString(obj);
 
-        if (StringUtil)
+        if (StringUtil.isEmpty(strValue)) {
+            return longVal;
+        }
+
+        try {
+            longVal = Long.parseLong(strValue);
+        } catch (NumberFormatException e) {
+            longVal = defaultValue;
+        }
+        return longVal;
     }
 
+    public static int castInt(Object object) {
+        return castInt(object, 0);
+    }
+
+    public static int castInt(Object obj, int defaultValue) {
+        int intVal = defaultValue;
+        if (obj == null) {
+            return intVal;
+        }
+
+        String strValue = CastUtil.castString(obj);
+        if (StringUtil.isNotEmpty(strValue)) {
+            try {
+                intVal = Integer.parseInt(strValue);
+            } catch (NumberFormatException e) {
+                intVal = defaultValue;
+            }
+        }
+
+        return intVal;
+    }
+
+    public static boolean castBoolean(Object object) {
+        return castBoolean(object, false);
+    }
+
+    public static boolean castBoolean(Object object, boolean defaultValue) {
+        boolean bValue = defaultValue;
+        if (object == null) {
+            return bValue;
+        }
+
+        String strValue = CastUtil.castString(object);
+        if (StringUtil.isNotEmpty(strValue)) {
+            try {
+                bValue = Boolean.parseBoolean(strValue);
+            } catch (NumberFormatException e) {
+                bValue = defaultValue;
+            }
+        }
+
+        return bValue;
+    }
 }
